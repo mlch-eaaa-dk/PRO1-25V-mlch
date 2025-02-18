44fb931458150c6e6bc7b070ee9687a8944c5c30
MLCH <mlch@eaaa.dk>
2025-02-18 10:52:34 +0100

    Add all examples from Workshop: Loops with objects
    All examples from the workshop is added, together with the loopstemplate folder.

---
+++ L09 Object use/src/example5/App.java
@@ -0,0 +1,7 @@
+package example5;
+
+public class App {
+	public static void main(String[] args) {
+		Gui.launch(Gui.class);
+	}
+}
---
+++ L09 Object use/src/loopstemplate/App.java
@@ -0,0 +1,7 @@
+package loopstemplate;
+
+public class App {
+	public static void main(String[] args) {
+		Gui.launch(Gui.class);
+	}
+}
